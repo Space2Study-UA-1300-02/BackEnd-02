99fb84b827fa8ae6599dc84c4674334611723151
JSTrainee <eternal@disroot.org>
2025-02-06 02:03:35 +0200

    app.js repair (after swagger)

--- src/initialization/serverSetup.js
+++ src/initialization/serverSetup.js
@@ -7,17 +7,27 @@ const {
 } = require('~/configs/config')
 const scheduledCronJobs = require('~/cron-jobs/scheduledCronJobs')
 
-const serverSetup = async (app) => {
+const serverSetup = async (app, { swaggerUi, swaggerSpec } = {}) => {
   await databaseInitialization()
   await checkUserExistence()
+
+  if (swaggerUi && swaggerSpec) {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  }
+
   initialization(app)
-  return app.listen(SERVER_PORT, () => {
+
+  const server = app.listen(SERVER_PORT, () => {
     logger.info(`Server is running on http://localhost:${SERVER_PORT}/`)
-    logger.info(`Swagger UI: http://localhost:${SERVER_PORT}/api-docs`)
+    if (swaggerUi) {
+      logger.info(`Swagger UI: http://localhost:${SERVER_PORT}/api-docs`)
+    }
     if (process.env.NODE_ENV !== 'test') {
       scheduledCronJobs()
     }
   })
+
+  return server
 }
 
 module.exports = serverSetup
