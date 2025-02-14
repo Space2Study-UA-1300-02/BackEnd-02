9d105e6f1c34bd440c013ad84b22cc0ceb638f14
JSTrainee <eternal@disroot.org>
2025-02-14 04:37:59 +0200

    upload-user

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,30 +1,44 @@
-// controllers/upload.js
-
 const categoryService = require('../services/category')
 const subjectService = require('../services/subject')
+const userService = require('../services/user')
 
 const uploadAndUpdate = async (req, res) => {
   try {
-    const { type, id } = req.body
+    const { type, id, email } = req.body
 
     if (!req.file) {
       return res.status(400).json({ error: 'File not uploaded' })
     }
 
     const imageUrl = req.file.path
-
     let result
-    const updateData = {
-      appearance: {
-        icon: imageUrl
-      }
-    }
 
-    if (type === 'category') {
-      result = await categoryService.updateCategory(id, updateData)
-    } else if (type === 'subject') {
-      result = await subjectService.updateSubject(id, updateData)
-    } else {
+    switch (type) {
+    case 'category':
+      result = await categoryService.updateCategory(id, {
+        appearance: { icon: imageUrl }
+      })
+      break
+
+    case 'subject':
+      result = await subjectService.updateSubject(id, {
+        appearance: { icon: imageUrl }
+      })
+      break
+
+    case 'user':
+      if (!email) {
+        return res.status(400).json({ error: 'Email is required for user updates' })
+      }
+      const user = await userService.getUserByEmail(email)
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' })
+      }
+      await userService.privateUpdateUser(user._id, { photo: imageUrl })
+      result = { photo: imageUrl }
+      break
+
+    default:
       return res.status(400).json({ error: 'Invalid type' })
     }
 
