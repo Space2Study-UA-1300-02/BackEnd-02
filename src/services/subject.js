e97b1e722627afa69147ecaf948c78c98ca31e01
ANOVIKOVA21 <82145323+ANOVIKOVA21@users.noreply.github.com>
2025-02-18 18:31:21 +0200

    updated response (#21)

--- src/services/subject.js
+++ src/services/subject.js
@@ -7,10 +7,9 @@ const getSubjects = async () => {
   return Subject.find()
 }
 
-
 const getSubjectNames = async () => {
   const subjects = await Subject.find({}, 'name')
-  return subjects.map(subject => subject.name)
+  return subjects.map((subject) => subject.name)
 }
 
 const getSubjectById = async (id) => {
@@ -26,16 +25,22 @@ const getSubjectsByCategoryId = async (categoryId) => {
   } else if (!isNaN(categoryId)) {
     query.id = Number(categoryId)
   } else {
-    throw new Error("Invalid category ID format");
+    throw new Error('Invalid category ID format')
   }
-  const category = await Category.findOne(query).select("id");
+  const category = await Category.findOne(query).select('id')
   if (!category) {
-    throw new Error("Category not found");
+    throw new Error('Category not found')
   }
-  const subjects = await Subject.find({ categoryId: category.id }, "name");
-  return { items: subjects.map((subject) => subject.name) };
+  const subjects = await Subject.find({ categoryId: category.id }, 'name')
+  return {
+    items: subjects.map((subject) => {
+      return {
+        name: subject.name,
+        _id: subject._id
+      }
+    })
+  }
 }
-
 
 const createSubject = async (subjectData) => {
   const lastSubject = await Subject.findOne().sort('-id')
@@ -56,11 +61,7 @@ const updateSubject = async (id, updateData) => {
     updateFields['appearance.color'] = updateData.appearance.color
   }
 
-  const subject = await Subject.findOneAndUpdate(
-    { id },
-    { $set: updateFields },
-    { new: true }
-  )
+  const subject = await Subject.findOneAndUpdate({ id }, { $set: updateFields }, { new: true })
 
   if (!subject) throw DOCUMENT_NOT_FOUND('Subject')
   return subject
